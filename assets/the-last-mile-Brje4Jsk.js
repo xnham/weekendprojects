3470b85aab413480be026e8387488775a0e2e2c6
xnham <35735154+xnham@users.noreply.github.com>
2025-03-29 12:40:00 +0000

    Deploying to gh-pages from @ xnham/weekendprojects@ba45ff4ec71347cbeaa1d7502c4e4e49ec6da722 🚀

--- assets/the-last-mile-Brje4Jsk.js
+++
@@ -1,1 +0,0 @@
-import"./index-Cvow2rbT.js";const t={title:"The last mile",description:"All the tools are there but no one at a tennis club can put them together.",date:"2025-03-11",published:!1,image:null,tags:[],exceprt:null},{title:l,description:i,date:n,published:s,image:o,tags:c,exceprt:r}=t;function d(e){}export{d as default,t as metadata};
