372311266306e7d11a746cfea036978d652f024f
xnham <35735154+xnham@users.noreply.github.com>
2025-04-11 11:13:06 +0000

    Deploying to gh-pages from @ xnham/weekendprojects@fe13be233ac2945217d6906f96d711e1fe827391 🚀

---
+++ assets/bye-bye-subscriptions-k8cH4q1V.js
@@ -0,0 +1,1 @@
+import"./index-CkjRYhqR.js";const e={title:"Bye bye subscriptions",description:"Bye bye subscriptions. Use Webflow as an example.",date:"2025-03-11",published:!1,excerpt:null},{title:i,description:a,date:n,published:o,excerpt:p}=e;function c(t){}export{c as default,e as metadata};
