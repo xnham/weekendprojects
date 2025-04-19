71f20407e99d4e3f4c92d4387c148dc9a1a16fb4
Wendy Ham <wendyham@gmail.com>
2025-04-19 08:54:44 -0400

    Add sunny.gif

--- src/components/AboveTheFold.svelte
+++ src/components/AboveTheFold.svelte
@@ -64,7 +64,7 @@
 
         <p>I'm curious: Have the economic forces now enabled, maybe even favored, hyperpersonal software creation? Can someone with my patchy technical background build software that's actually worth using?</p>
 
-        <p>Btw, one of the things I made was an AI voice agent called Sunny, designed to help spark project ideas. If you'd love to simplify some aspects of your life, or just see what talking to a robot is like, how about <a href="/ms-florentina">giving it a call</a>?</p>
+        <p>Btw, one of the things I made was an AI voice agent called Sunny, designed to help spark project ideas. If you'd love to simplify some aspects of your life, or just see what talking to a robot is like, how about <a href="/sunny">giving it a call</a>?</p>
 
       </div>
     </div>
