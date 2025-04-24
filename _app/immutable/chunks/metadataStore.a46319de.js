d31d2cc8669da7858280981fb4954477b9b4fb2e
xnham <35735154+xnham@users.noreply.github.com>
2025-04-24 15:36:44 +0000

    Deploying to gh-pages from @ xnham/weekendprojects@13deb23e235aa7b23f69942548418e557e4957fd 🚀

---
+++ _app/immutable/chunks/metadataStore.a46319de.js
@@ -0,0 +1,1 @@
+import{w as a}from"./index.c4045f35.js";const o=a({title:"Wendy Ham | Software for One",description:"Building software for one — personalized, useful, playful projects that solve human-sized problems.",canonicalUrl:"https://xnham.com",type:"website",url:""});function s(e){o.update(t=>({...t,...e}))}export{o as m,s as u};
